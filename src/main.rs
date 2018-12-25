1e54de9e04d7a50adb659770a127bb5f2dcbe4fa
XX <freecoder.xx@gmail.com>
2018-12-25 23:10:35 +0300

    Fix version linkage comments

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use clap::{App, Arg, SubCommand};
 use cmd::{Cmd, CmdProcessor};
 use settings::Settings;
 
-const VERSION: &'static str = "0.6.0"; // Related with `version` value in Cargo.toml
+const VERSION: &'static str = "0.6.0"; // Same as `version` value in Cargo.toml
 
 fn main() {
     let settings = Settings::new().expect("Read settings error");
